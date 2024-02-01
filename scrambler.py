2475aadb895e961c1c2ac0b5c99308fdc8118554
ScidaJ <scidaj@gmail.com>
2024-02-01 13:40:59 -0500

    Adjusted test to function properly. Added pre-commit hooks and black linter.

--- scrambler.py
+++ scrambler.py
@@ -7,12 +7,13 @@ from pathlib import Path
 from sys import exit
 
 DIR = getcwd()
-OUT_DIR_PATH = '\out'
-MAP_LIST_FILE = '\maps.txt'
-SERVER_FILE_COPY = '\server_copy.cfg'
+OUT_DIR_PATH = "\out"
+MAP_LIST_FILE = "\maps.txt"
+SERVER_FILE_COPY = "\server_copy.cfg"
 
-def main(): 
-    
+
+def main():
+
     args = prase_args()
 
     validate_args(args)
@@ -21,73 +22,139 @@ def main():
     random_map_list = {}
     output_file = None
     server_file = None
-    output_file_path = ''
-    output_file_name = args.OutputFile if args.OutputFile is not None else SERVER_FILE_COPY
-    map_string = ''
-    map_file = Path(args.MapList).read_text().split('\n')
+    output_file_path = ""
+    output_file_name = (
+        args.OutputFile if args.OutputFile is not None else SERVER_FILE_COPY
+    )
+    map_string = ""
+    map_file = Path(args.MapList).read_text().split("\n")
     server_file_exists = False
 
     map_filter = ingest_filter(args)
 
-    #ingest_filter(args) will alter the final size of the map list, so it must be calculated after the filter is generated.
-    size = (len(map_file) - 1) if map_filter == '' else ((len(map_file) - 1) - len(map_filter.split(',')))
+    # ingest_filter(args) will alter the final size of the map list, so it must be calculated after the filter is generated.
+    size = (
+        (len(map_file) - 1)
+        if map_filter == ""
+        else ((len(map_file) - 1) - len(map_filter.split(",")))
+    )
 
     random_map_list_builder(map_filter, map_file, maps, random_map_list, size)
-    
-    print('Maps have been scrambled, writing to', 'list file' if args.ServerFile is None else 'server file')
+
+    print(
+        "Maps have been scrambled, writing to",
+        "list file" if args.ServerFile is None else "server file",
+    )
 
     if args.ServerFile is not None:
         server_file_exists = exists(args.ServerFile)
-    
+
     if args.ServerFile is not None and not server_file_exists:
-        print('Your server file', args.ServerFile, 'does not exist. Check the file path.')
+        print(
+            "Your server file", args.ServerFile, "does not exist. Check the file path."
+        )
         input()
         exit()
     elif server_file_exists:
         if args.OutputDir is not None:
-             output_file_path = f'{args.OutputDir}{output_file_name}'
+            output_file_path = f"{args.OutputDir}{output_file_name}"
         elif not args.override:
-            output_file_path = f'{DIR}{OUT_DIR_PATH}{output_file_name}'
+            output_file_path = f"{DIR}{OUT_DIR_PATH}{output_file_name}"
         else:
             output_file_path = args.ServerFile
-        server_file = open(args.ServerFile, 'r+')
-        output_file = open(output_file_path, 'w+') if not output_file_path is args.ServerFile else server_file
+        server_file = open(args.ServerFile, "r+")
+        output_file = (
+            open(output_file_path, "w+")
+            if not output_file_path is args.ServerFile
+            else server_file
+        )
     else:
-        print('Creating directory and map list file if it does not exist.')
+        print("Creating directory and map list file if it does not exist.")
         args.ServerFile = None
-        Path(f'{DIR}{OUT_DIR_PATH}').mkdir(parents=True, exist_ok=True)
-        output_file_path = f'{DIR}{OUT_DIR_PATH}{MAP_LIST_FILE}'
-        output_file = open(output_file_path, 'w+')
-    
+        Path(f"{DIR}{OUT_DIR_PATH}").mkdir(parents=True, exist_ok=True)
+        output_file_path = f"{DIR}{OUT_DIR_PATH}{MAP_LIST_FILE}"
+        output_file = open(output_file_path, "w+")
+
     if output_file is not None:
         map_string = string_builder(args, random_map_list, maps, server_file)
         output_file.seek(0)
         output_file.write(map_string)
         output_file.truncate()
-        print('New', 'server file' if server_file_exists else 'map list', 'generated! Find your new file here:', output_file_path, '\nThe first map is', random_map_list[0])
+        print(
+            "New",
+            "server file" if server_file_exists else "map list",
+            "generated! Find your new file here:",
+            output_file_path,
+            "\nThe first map is",
+            random_map_list[0],
+        )
     else:
-        raise Exception('final_map_file not opened. Contact dev for assistance here https://github.com/ScidaJ/MapScramblerPython')
+        raise Exception(
+            "final_map_file not opened. Contact dev for assistance here https://github.com/ScidaJ/MapScramblerPython"
+        )
 
-def prase_args(argv = None):
-    parser = argparse.ArgumentParser(prog='scrambler.py')
-    parser.add_argument('MapList', help='Path to the map list that you want to use with the scrambler. Will not create if it does not exist.')
-    parser.add_argument('--ServerFile', help='Path to the server startup file or the file which contains the map list. Will not create if it does not exist. Do not use with --list.')
-    parser.add_argument('--PreArg', help='Arguments that go before the map list. Spaces/New lines will not be added to the end.')
-    parser.add_argument('--PostArg', help='Arguments that go after the map list. Spaces/New lines will not be added to the front.')
-    parser.add_argument('--ArgSliceChar', help='Character to slice the server arguments by. Must be exactly 2 slice characters in server file, One directly before the map list, and one directly after. CANNOT be used with --PreArg or --PostArg.')
-    parser.add_argument('--OutputDir', help='The directory where you would like to output the final copy. Not to be used with -o.')
-    parser.add_argument('--OutputFile', help='The name of the output file. Not to be used with -o.')
-    parser.add_argument('--Filter', help='Provide a CSV list of maps or path to a file containing one to be filtered out of the scramble.', default='')
-    parser.add_argument('-o', '--override', help='Overwrites original server.cfg with scrambled version. Only use this if you know what you are doing. Not to be used with -l.', action='store_true')
-    parser.add_argument('-p', '--prefix', help='Prefix for map names. Space will not be added if they exist. E.g. mp [mapName]')
-    parser.add_argument('-q', '--quotes', help='Encapsulates the map list in quotes.', action='store_true')
-    parser.add_argument('-v', '--verbose', help='Enables verbose output.', action='store_true')
+
+def prase_args(argv=None):
+    parser = argparse.ArgumentParser(prog="scrambler.py")
+    parser.add_argument(
+        "MapList",
+        help="Path to the map list that you want to use with the scrambler. Will not create if it does not exist.",
+    )
+    parser.add_argument(
+        "--ServerFile",
+        help="Path to the server startup file or the file which contains the map list. Will not create if it does not exist. Do not use with --list.",
+    )
+    parser.add_argument(
+        "--PreArg",
+        help="Arguments that go before the map list. Spaces/New lines will not be added to the end.",
+    )
+    parser.add_argument(
+        "--PostArg",
+        help="Arguments that go after the map list. Spaces/New lines will not be added to the front.",
+    )
+    parser.add_argument(
+        "--ArgSliceChar",
+        help="Character to slice the server arguments by. Must be exactly 2 slice characters in server file, One directly before the map list, and one directly after. CANNOT be used with --PreArg or --PostArg.",
+    )
+    parser.add_argument(
+        "--OutputDir",
+        help="The directory where you would like to output the final copy. Not to be used with -o.",
+    )
+    parser.add_argument(
+        "--OutputFile", help="The name of the output file. Not to be used with -o."
+    )
+    parser.add_argument(
+        "--Filter",
+        help="Provide a CSV list of maps or path to a file containing one to be filtered out of the scramble.",
+        default="",
+    )
+    parser.add_argument(
+        "-o",
+        "--override",
+        help="Overwrites original server.cfg with scrambled version. Only use this if you know what you are doing. Not to be used with -l.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-p",
+        "--prefix",
+        help="Prefix for map names. Space will not be added if they exist. E.g. mp [mapName]",
+    )
+    parser.add_argument(
+        "-q",
+        "--quotes",
+        help="Encapsulates the map list in quotes.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "-v", "--verbose", help="Enables verbose output.", action="store_true"
+    )
 
     return parser.parse_args(argv)
 
+
 def random_map_list_builder(map_filter, map_file, maps, random_map_list, size):
     for curr_map in map_file:
-        curr_map = curr_map.split(',')
+        curr_map = curr_map.split(",")
         if curr_map[1] not in map_filter:
             maps[curr_map[1]] = curr_map[0]
             index = random.randint(0, size)
@@ -95,18 +162,22 @@ def random_map_list_builder(map_filter, map_file, maps, random_map_list, size):
                 index = random.randint(0, size)
             random_map_list[index] = curr_map[1]
 
+
 def ingest_filter(args):
-    if len(args.Filter) != 0 and '.txt' in args.Filter[(len(args.Filter) - 4):]:
-            map_filter = Path(args.Filter).read_text()
-            return map_filter
+    if len(args.Filter) != 0 and ".txt" in args.Filter[(len(args.Filter) - 4) :]:
+        map_filter = Path(args.Filter).read_text()
+        return map_filter
     else:
         return args.Filter
 
-def string_builder(args, random_map_list: dict, map_list: dict, server_file: TextIOWrapper) -> str:
+
+def string_builder(
+    args, random_map_list: dict, map_list: dict, server_file: TextIOWrapper
+) -> str:
     size = len(random_map_list) - 1
     file_slices = None
-    maps = ''
-    map_prefix = args.prefix if args.prefix is not None else ''
+    maps = ""
+    map_prefix = args.prefix if args.prefix is not None else ""
 
     if args.ArgSliceChar is not None and server_file is not None:
         file_slices = server_file.read().split(args.ArgSliceChar)
@@ -115,14 +186,16 @@ def string_builder(args, random_map_list: dict, map_list: dict, server_file: Tex
         maps += args.PreArg
 
     if args.quotes:
-        maps += '\"'
+        maps += '"'
 
     for i in range(size):
-        next_map = map_prefix + map_list[random_map_list[i]] + (' '  if i < size - 1 else '')
-        maps = ''.join((maps, next_map))
+        next_map = (
+            map_prefix + map_list[random_map_list[i]] + (" " if i < size - 1 else "")
+        )
+        maps = "".join((maps, next_map))
 
     if args.quotes:
-        maps += '\"'
+        maps += '"'
 
     if args.ArgSliceChar is not None and server_file is not None:
         maps += file_slices[2]
@@ -131,45 +204,57 @@ def string_builder(args, random_map_list: dict, map_list: dict, server_file: Tex
 
     return maps
 
+
 def validate_args(args):
     valid = True
 
     if not exists(args.MapList):
-        print('Your map file {0} does not exist. Exiting.'.format(args.MapList))
+        print("Your map file {0} does not exist. Exiting.".format(args.MapList))
         valid = False
 
     if args.override:
-        print('--override is set. Exit program with Ctrl + C if this is not correct, otherwise hit Enter.')
+        print(
+            "--override is set. Exit program with Ctrl + C if this is not correct, otherwise hit Enter."
+        )
         input()
 
     if args.override and args.ServerFile is None:
-        print('--override provided with no --ServerFile, this is not allowed. Exiting.')
+        print("--override provided with no --ServerFile, this is not allowed. Exiting.")
         valid = False
 
     if args.ArgSliceChar is not None and args.ServerFile is None:
-        print('ArgSliceChar specified but no ServerFile given. Ignoring ArgSliceChar. Please remove ArgSliceChar in the future.')
+        print(
+            "ArgSliceChar specified but no ServerFile given. Ignoring ArgSliceChar. Please remove ArgSliceChar in the future."
+        )
         input()
 
-    if args.ArgSliceChar is not None and args.ServerFile is not None and (args.PreArg is not None or args.PostArg is not None):
-        print('ArgSliceChar, ServerFile, PreArg or PostArg all defined. This is not allowed. Exiting.')
+    if (
+        args.ArgSliceChar is not None
+        and args.ServerFile is not None
+        and (args.PreArg is not None or args.PostArg is not None)
+    ):
+        print(
+            "ArgSliceChar, ServerFile, PreArg or PostArg all defined. This is not allowed. Exiting."
+        )
         valid = False
 
     if args.OutputDir is not None and args.override:
-        print('--OutputDir cannot be used with -o. Exiting.')
+        print("--OutputDir cannot be used with -o. Exiting.")
         valid = False
 
     if args.OutputFile is not None and args.override:
-        print('--OutputFile cannot be used with -o. Exiting.')
+        print("--OutputFile cannot be used with -o. Exiting.")
         valid = False
-    
-    if '.txt' in args.Filter[(len(args.Filter) - 4):] and not exists(args.Filter):
-        print('The filter file you provided does not exist. Exiting.')
+
+    if ".txt" in args.Filter[(len(args.Filter) - 4) :] and not exists(args.Filter):
+        print("The filter file you provided does not exist. Exiting.")
         valid = False
 
     if not valid:
-        print('Press Enter to exit.')
+        print("Press Enter to exit.")
         input()
         exit()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- scrambler_test.py
+++ scrambler_test.py
@@ -2,45 +2,55 @@ import unittest
 from pathlib import Path
 import scrambler
 
-MAP_LIST = './test/map_list_test.txt'
+MAP_LIST = "./test/map_list_test.txt"
 MAP_LIST_SIZE = 5
-FILTER = './test/map_list_filter_test.txt'
+FILTER = "./test/map_list_filter_test.txt"
+
 
 class TestScramblerFunctions(unittest.TestCase):
 
     def test_ingest_filter_file(self):
-        my_argv = [MAP_LIST, '--Filter', FILTER]
+        my_argv = [MAP_LIST, "--Filter", FILTER]
         args = scrambler.prase_args(my_argv)
-        expected = 'dMap 1,vmap1,pmap 1'
+        expected = "dMap 1,smap_3,pmap 1"
         result = scrambler.ingest_filter(args)
         self.assertEqual(result, expected)
 
     def test_ingest_filter_arguments(self):
-        my_argv = [MAP_LIST, '--Filter', 'dMap 1,vmap1,pmap 1']
+        my_argv = [MAP_LIST, "--Filter", "dMap 1,vmap1,pmap 1"]
         args = scrambler.prase_args(my_argv)
-        expected = 'dMap 1,vmap1,pmap 1'
+        expected = "dMap 1,vmap1,pmap 1"
         result = scrambler.ingest_filter(args)
         self.assertEqual(result, expected)
 
     def test_random_map_list_builder_no_filter(self):
         my_argv = [MAP_LIST]
         args = scrambler.prase_args(my_argv)
-        map_file = Path(MAP_LIST).read_text().split('\n')
+        map_file = Path(MAP_LIST).read_text().split("\n")
         maps = {}
         random_map_list = {}
-        maps_expected = {'Map 1': 'dmap_1', 'Map 2': 'smap_2', 'map 3': 'sMap_3', 'vmap1': 'vmap1', 'pmap 1': 'pmap_1', 'pmap 2': 'pmap_2'}
-        scrambler.random_map_list_builder('', map_file, maps, random_map_list, MAP_LIST_SIZE)
+        maps_expected = {
+            "dMap 1": "dmap_1",
+            "sMap 2": "smap_2",
+            "smap_3": "sMap_3",
+            "vmap1": "vmap1",
+            "pmap 1": "pmap_1",
+            "pmap 2": "pmap_2",
+        }
+        scrambler.random_map_list_builder(
+            "", map_file, maps, random_map_list, MAP_LIST_SIZE
+        )
         self.assertDictEqual(maps, maps_expected)
         self.assertEqual(len(maps), max(list(random_map_list.keys())) + 1)
 
     def test_random_map_list_builder_filters(self):
-        my_argv = [MAP_LIST, '--Filter', 'dMap 1,vmap1,pmap 1']
+        my_argv = [MAP_LIST, "--Filter", "dMap 1,vmap1,pmap 1"]
         args = scrambler.prase_args(my_argv)
-        filter = ['dMap 1', 'vmap1', 'pmap 1']
-        map_file = Path(MAP_LIST).read_text().split('\n')
+        filter = ["dMap 1", "vmap1", "pmap 1"]
+        map_file = Path(MAP_LIST).read_text().split("\n")
         maps = {}
         random_map_list = {}
-        scrambler.random_map_list_builder('', map_file, maps, random_map_list, MAP_LIST_SIZE)
+        maps_expected = {"sMap 2": "smap_2", "smap_3": "sMap_3", "pmap 2": "pmap_2"}
+        scrambler.random_map_list_builder(filter, map_file, maps, random_map_list, 2)
         self.assertEqual(len(maps), max(list(random_map_list.keys())) + 1)
-        for e in filter:
-            self.assertNotIn(e, random_map_list)
+        self.assertDictEqual(maps, maps_expected)
